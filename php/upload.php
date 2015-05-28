4c514a7df9dc5a8e9db0ff6112980682ea6b938c
Steve Peichel <speichel@ceg-engineers.com>
2015-05-28 08:36:06 -0500

    Changed from set path, to path selected

--- php/upload.php
+++ php/upload.php
@@ -21,7 +21,56 @@
 
 	
 	
+$target_dir = "../../../examples/line-time-series/uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is OK " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is NOT OK. ";
+        $uploadOk = 0;
+    }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+
+    echo "File already exists. ";
+    $uploadOk = 0;
 	
+}
+
+// Check file size
+//if ($_FILES["fileToUpload"]["size"] > 500000) {
+    //echo "Sorry, your file is too large.";
+    //$uploadOk = 0;
+//}
+// Allow certain file formats
+//if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+//&& $imageFileType != "gif" ) {
+    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    //$uploadOk = 0;
+//}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Your file was not uploaded. ";
+		
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 	
 	
 	
@@ -33,7 +82,7 @@
 	$file_prepped_for_changing = date('Y-m-d_') . $hour . date('-i-s')  .  ".txt";
 	//END - Code naming a file the current date and time
 	
-	$automatically_populated_textfile = file_get_contents("../../../examples/line-time-series/BigBlueRAW.txt");  //this will be the raw file in the future, hopefully automatically saved in some location
+	$automatically_populated_textfile = file_get_contents($target_file);  //this will be the raw file in the future, hopefully automatically saved in some location
 	//define('DIRECTORY_SEPARATOR','//');
 	//$path='uploads'.DIRECTORY_SEPARATOR;
 
@@ -119,61 +168,4 @@ $result3 = mysql_query($queryreduceduplicates) or trigger_error('Query MySQL Err
 
 
 
-
-
-
-
-
-$target_dir = "../../../examples/line-time-series/uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-
-
-
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-	
-}
-
-// Check file size
-//if ($_FILES["fileToUpload"]["size"] > 500000) {
-    //echo "Sorry, your file is too large.";
-    //$uploadOk = 0;
-//}
-// Allow certain file formats
-//if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-//&& $imageFileType != "gif" ) {
-    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    //$uploadOk = 0;
-//}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-		
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
 ?>
